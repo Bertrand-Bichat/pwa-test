717ae1082885e8dffbd8d92f4487d1ae09677140
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-06-18 18:49:29 +0200

    hide push notification ask button if permission is already set to granted or denied

--- app/javascript/plugins/push_notifications.js
+++ app/javascript/plugins/push_notifications.js
@@ -1,13 +1,20 @@
 const askPushNotifications = () => {
   const pushButton = document.querySelector('#push-permission');
 
-  pushButton?.addEventListener('click', () => {
+  pushButton?.addEventListener('click', (event) => {
+    event.preventDefault();
     window.Notification.requestPermission().then(permission => {
       if(permission !== 'granted'){
         throw new Error('Permission not granted for Notification');
+      } else if (permission !== 'default') {
+        pushButton.classList.add('hidden');
       }
     });
   });
+
+  if (Notification.permission !== 'default') {
+    pushButton.classList.add('hidden');
+  }
 
 }
 
