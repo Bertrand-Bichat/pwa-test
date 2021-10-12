2c55753af498caf51af94ced6aadde64ebd77a61
Bertrand Bichat <bertrand.bichat@gmail.com>
2021-10-12 19:24:28 +0200

    details

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -59,12 +59,12 @@ class PostsController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:title, :content)
-    end
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
